c874f54ff93976a635669affb80191d81b04ef95
Hongbin Qu <hongbq@uw.edu>
2018-12-11 23:50:53 -0800

    add modified files

--- pisa_analysis_tool/mapdata.py
+++ pisa_analysis_tool/mapdata.py
@@ -5,41 +5,43 @@ Processing map data
 
 import pandas as pd
 import folium
-from pisa_analysis_tool.preprocessing import make_country_dict
-from pisa_analysis_tool.preprocessing import add_lat_long
-from pisa_analysis_tool.preprocessing import remove_country
+from preprocessing import make_country_dict, add_lat_long, remove_country
+
 
 # json file with countries' name and their geographical data
-country_geo = '../../data/world_ogr.json'
+COUNTRY_GEO = '../data/world_ogr.json'
 # gender coefficient data for countries
-gender_coeff = '../../data/gender_coef.csv'
+GENDER_COEFF = '../data/gender_coef.csv'
 
 # preprocessing raw data
-female_avg_csv = '../../data/world_score_female_avg.csv'
-male_avg_csv = '../../data/world_score_male_avg.csv'
-country_avg_csv = '../../data/world_score_avg.csv'
+F_AVG_CSV = '../data/world_score_female_avg.csv'
+M_AVG_CSV = '../data/world_score_male_avg.csv'
+C_AVG_CSV = '../data/world_score_avg.csv'
 
-country_dict = make_country_dict()
+COUNTRY_DICT = make_country_dict()
 
-male_avg_dict, country_lst = add_lat_long(male_avg_csv, country_dict)
-female_avg_dict, country_lst1 = add_lat_long(female_avg_csv, country_dict)
-country_avg_dict, country_lst2 = add_lat_long(country_avg_csv, country_dict)
 
-df_male_avg = pd.DataFrame.from_dict(male_avg_dict)
-df_female_avg = pd.DataFrame.from_dict(female_avg_dict)
-df_country_avg = pd.DataFrame.from_dict(country_avg_dict)
+M_AVG_DICT, COUNTRY_LST = add_lat_long(M_AVG_CSV, COUNTRY_DICT)
+F_AVG_DICT, COUNTRY_LST1 = add_lat_long(F_AVG_CSV, COUNTRY_DICT)
+C_AVG_DICT, COUNTRY_LST2 = add_lat_long(C_AVG_CSV, COUNTRY_DICT)
 
-needed_country = remove_country(gender_coeff, country_dict, country_lst)
-indicator_data = pd.DataFrame.from_dict(needed_country)
+DF_M_AVG = pd.DataFrame.from_dict(M_AVG_DICT)
+DF_F_AVG = pd.DataFrame.from_dict(F_AVG_DICT)
+DF_C_AVG = pd.DataFrame.from_dict(C_AVG_DICT)
+
+NEED_COUNTRY = remove_country(GENDER_COEFF, COUNTRY_DICT, COUNTRY_LST)
+DF_INDICATOR = pd.DataFrame.from_dict(NEED_COUNTRY)
+
 
 # initalize the position when the html file is open
-m1 = folium.Map(location=[50, 15], zoom_start=1.5)
+M1 = folium.Map(location=[50, 15], zoom_start=1.5)
+
 
 # Add the color for the chloropleth:
-m1.choropleth(
-    geo_data=country_geo,
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Male Average Mathematics Score for Each Country',
-    data=df_male_avg,
+    data=DF_M_AVG,
     columns=['CountryName', 'Mathematics'],
     key_on='properties.NAME',
     fill_color='YlOrBr',
@@ -47,11 +49,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Male Average Mathematics Score'
-)  # Male Average Mathematics Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Male Average Mathematics Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Male Average Science Score for Each Country',
-    data=df_male_avg,
+    data=DF_M_AVG,
     columns=['CountryName', 'Reading'],
     key_on='properties.NAME',
     fill_color='PuBu',
@@ -59,11 +61,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Male Average Science Score'
-)  # Male Average Science Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Male Average Science Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Male Average Reading Score for Each Country',
-    data=df_male_avg,
+    data=DF_M_AVG,
     columns=['CountryName', 'Science'],
     key_on='properties.NAME',
     fill_color='GnBu',
@@ -71,11 +73,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Male Average Reading Score'
-)  # Male Average Reading Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Male Average Reading Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Female Average Mathematics Score for Each Country',
-    data=df_female_avg,
+    data=DF_F_AVG,
     columns=['CountryName', 'Mathematics'],
     key_on='properties.NAME',
     fill_color='YlOrBr',
@@ -83,11 +85,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Female Average Mathematics Score'
-)  # Female Average Mathematics Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Female Average Mathematics Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Female Average Science Score for Each Country',
-    data=df_female_avg,
+    data=DF_F_AVG,
     columns=['CountryName', 'Reading'],
     key_on='properties.NAME',
     fill_color='PuRd',
@@ -95,11 +97,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Female Average Science Score'
-)  # Female Average Science Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Female Average Science Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Female Average Reading Score for Each Country',
-    data=df_female_avg,
+    data=DF_F_AVG,
     columns=['CountryName', 'Science'],
     key_on='properties.NAME',
     fill_color='YlOrRd',
@@ -107,11 +109,11 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='Female Average Reading Score'
-)  # Female Average Reading Score distribution layer
-m1.choropleth(
-    geo_data=country_geo,
+) # Female Average Reading Score distribution layer
+M1.choropleth(
+    geo_data=COUNTRY_GEO,
     name='Countries\' indicator score',
-    data=indicator_data,
+    data=DF_INDICATOR,
     columns=['CountryName', 'indicator'],
     key_on='properties.NAME',
     fill_color='YlOrRd',
@@ -119,15 +121,16 @@ m1.choropleth(
     line_opacity=0.2,
     nan_fill_color='#8c8c8c',
     legend_name='World Bank gender parity index(GPI)'
-)  # World Bank gender parity index(GPI) layer
+) # World Bank gender parity index(GPI) layer
+
 
 # add markers for each country in the csv file
-for i in range(0, len(df_female_avg)):
-    folium.Marker([df_female_avg.iloc[i]['lat'],
-                   df_female_avg.iloc[i]['lon']],
-                  popup=df_female_avg.iloc[i]['CountryName']).add_to(m1)
+for i in range(0, len(DF_F_AVG)):
+    folium.Marker([DF_F_AVG.iloc[i]['lat'],
+                   DF_F_AVG.iloc[i]['lon']],
+                  popup=DF_F_AVG.iloc[i]['CountryName']).add_to(M1)
 
-folium.LayerControl().add_to(m1)
+folium.LayerControl().add_to(M1)
 
 # Save to html
-m1.save('map_data.html')
+M1.save('map_data.html')
